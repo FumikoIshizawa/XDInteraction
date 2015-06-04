dc500a3e2b67d27225989b27cb19f54dfbe03655
FumikoIshizawa <FumikoIshizawa@FumikoIshizawa.local>
2015-06-04 19:21:30 +0900

    First commit

---
+++ XDInteraction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XDInteraction
+//
+//  Created by FumikoIshizawa on 2015/06/04.
+//  Copyright (c) 2015年 FumikoIshiawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XDInteraction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XDInteraction
+//
+//  Created by FumikoIshizawa on 2015/06/04.
+//  Copyright (c) 2015年 FumikoIshiawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
